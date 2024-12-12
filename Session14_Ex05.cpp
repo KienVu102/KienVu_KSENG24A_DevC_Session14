687c80bc4d1742cef308168d09ac2a20b80e3b35
KienVu102 <vudinhkien1002@gmail.com>
2024-12-12 09:51:45 +0700

    Kiên Vũ upload Session14_Ex05

---
+++ Session14_Ex05.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char str[1000];
+	int letter = 0;
+	printf("Moi ban nhap chuoi: ");
+	gets(str);
+	for(int i = 0; i < strlen(str); i++){
+		if(str[i] == ' '){
+			letter++;
+		}
+	}
+	printf("\nChuoi ban nhap co %d tu\n", letter+1);
+	return 0;
+}
