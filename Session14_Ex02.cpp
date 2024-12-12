cc1976528c4dae001074c37b6f5b936b54453e24
KienVu102 <vudinhkien1002@gmail.com>
2024-12-12 09:51:29 +0700

    Kiên Vũ upload Session14_Ex02

---
+++ Session14_Ex02.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char str[500];
+	printf("Nhap chuoi: ");
+	gets(str);
+	for(int i = 0; i < strlen(str); i++){
+		printf("%c ", str[i]);
+	}
+	return 0;
+}
